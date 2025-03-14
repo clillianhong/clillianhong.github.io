1c818143923b2eab2cf7c9039b97e302f8ed74e2
lillianhong <lillianhong@fb.com>
2025-03-14 07:51:44 -0400

    game assets - non lfs

--- craco.config.js
+++ craco.config.js
@@ -8,4 +8,26 @@ module.exports = {
             ],
         },
     },
+    webpack: {
+        configure: (webpackConfig) => {
+        // Add custom module rules
+        webpackConfig.module.rules.push({
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: /node_modules/,
+        });
+        webpackConfig.module.rules.push({
+            test: /\.esm\.js$/,
+            use: 'esm-loader',
+        });
+        return webpackConfig;
+        },
+    },
+    babel: {
+        presets: [
+        // ... other presets ...
+        '@babel/preset-react',
+        ],
+    },
+
 }
--- src/Containers/LandingPage/LandingPage.js
+++ src/Containers/LandingPage/LandingPage.js
@@ -4,12 +4,20 @@ import NavBar, { PageType } from "../../Components/NavBar"
 import Content from "../../Components/Content"
 import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 
 const LandingPage = () => {
 
     const [defaultTab, setDefaultTab] = useState(PageType.HOME)
     const [chosenTab, setChosenTab] = useState(PageType.HOME);
+
+    const { unityProvider } = useUnityContext({
+        loaderUrl: "../../unity/portfolio_game/Build/portfolio_game.loader.js",
+        dataUrl: "../../unity/portfolio_game/Build/portfolio_game.data",
+        frameworkUrl: "../../unity/portfolio_game/Build/portfolio_game.framework.js",
+        codeUrl: "../../unity/portfolio_game/Build/portfolio_game.wasm",
+    });
 
     const resetChosenTab = () => {
         setChosenTab(defaultTab);
@@ -29,6 +37,7 @@ const LandingPage = () => {
         <div className="flex h-1/6 w-full absolute inset-xy-0 top-0 items-center justify-center text-xs italic" >
             [instructions] click objects, drag for orbit camera, two finger swipe for zoom
         </div>
+        <Unity unityProvider={unityProvider} />
 
     </main>);
 }
